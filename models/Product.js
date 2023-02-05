9cb281cfb8fe7d4aa1c24788bfd68018b2823fb6
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-02-05 14:27:03 -0800

    Defined ProdcutTag  columns

--- models/Product.js
+++ models/Product.js
@@ -16,11 +16,33 @@ Product.init(
       primaryKey: true,
       autoIncrement: true 
     },
-      Category_name:{
+      product_name:{
         type: DataTypes.STRINGS,
         allowNull: false
+    },
+    price:{
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true
+      } 
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     }
   },
+
   {
     sequelize,
     timestamps: false,
